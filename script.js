4a58d987cdec2ae4e437c076e72705431df9f849
Sheng0o0 <shenghaozheng@mybihs.org>
2016-08-02 20:14:12 +0000

    done

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 $(document).ready(function(){
     
 
-
+$("body").keydown(function(event){
+    
+    if (event.keyCode===37){
+       $('#tom').attr("src","tom/tomLeft.jpg");
+    }else if(event.keyCode===38){
+        $('#tom').attr("src","tom/tomUp.jpg");
+    }else if(event.keyCode===39){
+        $('#tom').attr("src","tom/tomRight.jpg");
+    }else if(event.keyCode===40){
+        $('#tom').attr("src","tom/tomDown.jpg");
+    }
+    
+});
+$('body').keyup(function(event){
+    if (event.keyCode===37){
+       $('#tom').attr("src","tom/tomStand.jpg");
+    }else if(event.keyCode===38){
+        $('#tom').attr("src","tom/tomStand.jpg");
+    }else if(event.keyCode===39){
+        $('#tom').attr("src","tom/tomStand.jpg");
+    }else if(event.keyCode===40){
+        $('#tom').attr("src","tom/tomStand.jpg");
+    }
+});
     
     
 });
